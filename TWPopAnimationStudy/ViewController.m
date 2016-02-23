8176cea77f0e93a86a817c78b8d736d7e4494d66
wangdxnum1 <Wang_Wonderful@foxmail.com>
2016-02-23 14:30:40 +0800

    Facebook 的pop动画框架学习
    Facebook 的pop动画框架学习

--- TWPopAnimationStudy/ViewController.m
+++ TWPopAnimationStudy/ViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <pop/POP.h>
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *testView1;
 @end
 
 @implementation ViewController
@@ -24,4 +26,27 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    [self test3];
+}
+
+- (void)test1{
+    POPSpringAnimation *anim = [POPSpringAnimation animationWithPropertyNamed:kPOPLayerBounds];
+    anim.toValue = [NSValue valueWithCGRect:CGRectMake(0, 0, 200, 200)];
+    [self.testView1.layer pop_addAnimation:anim forKey:@"size"];
+}
+
+- (void)test2{
+    POPDecayAnimation *anim = [POPDecayAnimation animationWithPropertyNamed:kPOPLayerPositionX];
+    anim.velocity = @(300.);
+    [self.testView1.layer pop_addAnimation:anim forKey:@"slide"];
+}
+
+- (void)test3{
+    POPBasicAnimation *anim = [POPBasicAnimation animationWithPropertyNamed:kPOPViewAlpha];
+    anim.timingFunction = [CAMediaTimingFunction functionWithName:kCAMediaTimingFunctionEaseInEaseOut];
+    anim.fromValue = @(0.0);
+    anim.toValue = @(1.0);
+    [self.testView1 pop_addAnimation:anim forKey:@"fade"];
+}
 @end
